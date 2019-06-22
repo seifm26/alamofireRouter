fb4dc1eca62848e6810cc10e7b8a05392c2ff2c2
SourceKhone <sdfsdfsd@sdfsdfsds-Mac.local>
2019-06-22 09:31:27 -0700

    Initial Commit

---
+++ alamofireRouter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alamofireRouter
+//
+//  Created by SourceKhone on 6/22/19.
+//  Copyright © 2019 mrs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
